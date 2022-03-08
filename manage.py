39292466a8ea928841dfbf7de516e90a61628b2c
Tururocks101 <95218150+Tururocks101@users.noreply.github.com>
2022-03-08 17:18:29 +0300

    Add files via upload

---
+++ manage.py
@@ -0,0 +1,21 @@
+from app import create_app, db
+from flask_script import Manager, Server
+from app.models import Pitch, Category
+from flask_migrate import Migrate, MigrateCommand
+# creating app instance
+app = create_app('development')
+app = create_app('production')
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+manager.add_command('server', Server)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, Pitch=Pitch, Category=Category)
+
+
+if __name__ == '__main__':
+    manager.run()
