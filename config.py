10bdffe51f77bd38763f9c00c19dddf5029b9a18
Tururocks101 <95218150+Tururocks101@users.noreply.github.com>
2022-03-08 17:16:24 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://puppah:zanpakutou@localhost/pitch_hub'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
