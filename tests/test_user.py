e005550b174dca0563b92f5e706a6bf0b4de3bed
Tururocks101 <95218150+Tururocks101@users.noreply.github.com>
2022-03-08 17:32:32 +0300

    tests to the user password

---
+++ tests/test_user.py
@@ -0,0 +1,17 @@
+import unittest
+from app.models import User
+
+
+class UserTest(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User(password='bob')
+
+    def test_init(self):
+        self.assertTrue(self.new_user.password_encrypt is not None)
+
+    def test_alert(self):
+        with self.assertRaises(AttributeError):
+            self.new_user.password
+
+    def test_save(self):
+        self.assertTrue(self.new_user.check_password('bob'))
